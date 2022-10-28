8b0e8d7094adeb4cff08b023e90bbefeed25e706
chayansrk64 <chayansrk64@gmail.com>
2022-10-28 23:40:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".navbar__items");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "block";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display ="inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav);
+
